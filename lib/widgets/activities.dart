cfe980f30b1f6cb5447845a32a37354be2121f84
Jeron <norejudeisa@gmail.com>
2022-09-02 23:59:09 +0000

    create user balance widget

---
+++ lib/widgets/activities.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Activies extends StatefulWidget {
+  const Activies({super.key});
+
+  @override
+  State<Activies> createState() => _ActiviesState();
+}
+
+class _ActiviesState extends State<Activies> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
