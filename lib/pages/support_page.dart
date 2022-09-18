4af14993a811819443926259573e78d10c3a8349
Jeron <norejudeisa@gmail.com>
2022-09-18 23:02:43 +0000

    redesign support page

--- lib/pages/support_page.dart
+++ lib/pages/support_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vodafone/data/support_page_data.dart';
+import 'package:vodafone/theme/app.dart';
 
 class SupportPage extends StatelessWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -17,30 +18,40 @@ class SupportPage extends StatelessWidget {
         itemCount: supportPageData.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 6,
+          mainAxisSpacing: 15,
+          crossAxisSpacing: 15,
           childAspectRatio: 1 / 0.8,
         ),
         itemBuilder: (context, index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    supportPageData[index]['image'],
-                    width: 35,
-                    color: Colors.red,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Text(
-                      supportPageData[index]['title'],
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+          return InkWell(
+            onTap: () {},
+            child: Ink(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  Shadow.sm,
                 ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      supportPageData[index]['image'],
+                      width: 35,
+                      color: Colors.red,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: Text(
+                        supportPageData[index]['title'],
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
